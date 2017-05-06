bd23854cc002881b64f7e223e43583755191aa64
Tara <tbm239@gmail.com>
2017-05-06 17:59:41 +0000

    Yana, Ike, and Tara Hangman - WE DID IT

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -12,13 +12,16 @@ except NameError:
 
 
 # Internal helpers
+
+
+
 def _get_random_word(word_list):
     """
     Returns a random word from the word_list to use as the answer word.
     :param word_list: The list of possible answer words
     """
-    pass
-
+    answer_word = random.choice(word_list)
+    return answer_word
 
 def _mask_word(word):
     """
@@ -27,7 +30,13 @@ def _mask_word(word):
     :param word: The answer word for the current game
     Example: word - 'cat'     masked word - '***'
     """
-    pass
+    masked_word = ''
+    
+    for character in word:
+        masked_word += '*'
+        
+    return masked_word
+        
 
 
 def _guess_is_valid(guessed_letter, previous_guesses):
@@ -38,7 +47,11 @@ def _guess_is_valid(guessed_letter, previous_guesses):
     :param previous_guesses: A string of all the letters previously guessed
     Returns True if given guess is valid, False otherwise.
     """
-    pass
+    
+    if len(guessed_letter) == 1 and guessed_letter.isalpha() and guessed_letter not in previous_guesses:
+        return True
+    else:
+        return False
 
 
 def _check_lose(remaining_misses):
@@ -46,7 +59,10 @@ def _check_lose(remaining_misses):
     Returns True if remaining guesses is equal to 0 and false otherwise.
     :param remaining_misses: How many misses are left before user loses
     """
-    pass
+    if remaining_misses == 0:
+        return True
+    else:
+        return False
 
 
 def _check_win(answer_word, masked_word):
@@ -59,7 +75,11 @@ def _check_win(answer_word, masked_word):
     :param masked_word: The answer word masked with '*' characters for letters
                         that haven't been guessed
     """
-    pass
+    # Hint: (for this one, make sure you look at what arguments are being passed) :) Thanks
+    if answer_word == masked_word:
+        return True
+    else:
+        return False
 
 
 def _check_game_over(answer_word, masked_word, remaining_misses):
@@ -71,7 +91,10 @@ def _check_game_over(answer_word, masked_word, remaining_misses):
                         that haven't been guessed
     :param remaining_misses: How many misses are left before user loses
     """
-    pass
+    if answer_word == masked_word or remaining_misses == 0:
+        return True
+    else:
+        return False
 
 
 # Public interface
@@ -86,7 +109,15 @@ def start_new_game(word_list, answer_word=None):
     previous_guesses - The previous valid letter guesses the user has inputted
     remaining_misses - how many misses the user has left. Start with 5.
     """
-    pass
+    answer_word = answer_word or _get_random_word(word_list)
+    
+    game = {'answer_word' : answer_word,
+            'masked_word' : _mask_word(answer_word),
+            'previous_guesses' : '',
+            'remaining_misses' : 5,
+    }
+    
+    return game
 
 
 def guess_letter(game, letter):
@@ -104,8 +135,58 @@ def guess_letter(game, letter):
     :param game: The dictionary storing current game information
     :param letter: The letter that is being guessed
     """
-    pass
+    
+   # len(game[answer_word]) == len(game['masked_word']) 
+    
+    new_masked_string = ''
 
+    if _guess_is_valid(letter, game['previous_guesses']):
+        if letter in game['answer_word']:
+            for num in range(len(game['answer_word'])):
+                if game['answer_word'][num] == letter:
+                    new_masked_string += letter
+                else:
+                    new_masked_string += game['masked_word'][num]
+            game['masked_word'] = new_masked_string
+        else:
+            game['remaining_misses'] -= 1
+    
+    game['previous_guesses'] += letter
+    
+                    
+"""
+
+    def test_guess_invalid_nonalpha(self):
+>       self.assertEqual(_guess_is_valid('1', 'abcd'), False)
+E       AssertionError: None != False
+
+tests/test_main.py:24: AssertionError
+____________________________________________________________________________________________________ TestHangman.test_guess_invalid_repeat ____________________________________________________________________________________________________
+
+self = <test_main.TestHangman testMethod=test_guess_invalid_repeat>
+
+    def test_guess_invalid_repeat(self):
+>       self.assertEqual(_guess_is_valid('a', 'abcd'), False)
+E       AssertionError: None != False
+
+game['answer_word'] == 'mouse'
+game['masked_word'] == '*****'
+our guess -- u
+    
+    new_masked_sting = ""
+    if _guess_is_valid(letter, game['previous_guesses']):
+        if letter in game['answer_word']:
+            for num in range(len(game['answer_word'])):
+                if game['answer_word'][num] == letter:
+                    then new_masked_string += letter
+                else: 
+                    new masked_string += game['masked_word'][num]
+    
+    game['masked_string'] = new_masked_string
+
+new_masked_string = '**u**'
+
+"""
 
 def user_input_guess(game):
     """
