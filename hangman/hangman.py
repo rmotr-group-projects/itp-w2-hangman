c408fa2a4989eba689a6b78e60e07c9851040539
Dan Barnes <danbarnes12@gmail.com>
2017-06-10 02:23:12 +0000

    First Commit

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -17,7 +17,7 @@ def _get_random_word(word_list):
     Returns a random word from the word_list to use as the answer word.
     :param word_list: The list of possible answer words
     """
-    pass
+    return random.choice(word_list)
 
 
 def _mask_word(word):
@@ -27,7 +27,8 @@ def _mask_word(word):
     :param word: The answer word for the current game
     Example: word - 'cat'     masked word - '***'
     """
-    pass
+    word_length = len(word)
+    return '*'*word_length
 
 
 def _guess_is_valid(guessed_letter, previous_guesses):
@@ -38,16 +39,24 @@ def _guess_is_valid(guessed_letter, previous_guesses):
     :param previous_guesses: A string of all the letters previously guessed
     Returns True if given guess is valid, False otherwise.
     """
-    pass
-
+    if len(guessed_letter) != 1:
+        return False
+    elif guessed_letter in previous_guesses:
+        return False
+    elif guessed_letter.isalpha() == False:
+        return False
+    else: 
+        return True
 
 def _check_lose(remaining_misses):
     """
     Returns True if remaining guesses is equal to 0 and false otherwise.
     :param remaining_misses: How many misses are left before user loses
     """
-    pass
-
+    if remaining_misses == 0:
+        return True
+    else:
+        return False
 
 def _check_win(answer_word, masked_word):
     """
@@ -59,8 +68,10 @@ def _check_win(answer_word, masked_word):
     :param masked_word: The answer word masked with '*' characters for letters
                         that haven't been guessed
     """
-    pass
-
+    if answer_word == masked_word:
+        return True
+    else:
+        return False
 
 def _check_game_over(answer_word, masked_word, remaining_misses):
     """
@@ -71,8 +82,10 @@ def _check_game_over(answer_word, masked_word, remaining_misses):
                         that haven't been guessed
     :param remaining_misses: How many misses are left before user loses
     """
-    pass
-
+    if _check_win(answer_word,masked_word) == True or _check_lose(remaining_misses) == True:
+        return True
+    else:
+        return False
 
 # Public interface
 def start_new_game(word_list, answer_word=None):
@@ -86,8 +99,17 @@ def start_new_game(word_list, answer_word=None):
     previous_guesses - The previous valid letter guesses the user has inputted
     remaining_misses - how many misses the user has left. Start with 5.
     """
-    pass
-
+    
+    new_game = {'answer_word':'',
+                'masked_word':'',
+                'previous_guesses':'',
+                'remaining_misses': 5}
+    if answer_word == None:
+        new_game['answer_word'] = _get_random_word(word_list)
+    else:
+        new_game['answer_word'] = answer_word
+    new_game['masked_word'] = _mask_word(new_game['answer_word'])    
+    return new_game            
 
 def guess_letter(game, letter):
     """
@@ -104,8 +126,18 @@ def guess_letter(game, letter):
     :param game: The dictionary storing current game information
     :param letter: The letter that is being guessed
     """
-    pass
-
+    
+    masked_word_new = ''
+    if letter in game['answer_word']:
+        for num in range(len(game['answer_word'])):
+            if letter == game['answer_word'][num]:
+                masked_word_new += letter
+            else:
+                masked_word_new += game['masked_word'][num]
+        game['masked_word'] = masked_word_new    
+    else:
+        game['remaining_misses'] -= 1
+    game['previous_guesses'] += letter    
 
 def user_input_guess(game):
     """
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,6 @@ from hangman.hangman import (_get_random_word, _mask_word, _guess_is_valid,
                              _check_lose, _check_win, _check_game_over,
                              _check_game_over, start_new_game, guess_letter)
 
-
 class TestHangman(unittest.TestCase):
 
     def test_get_random_word(self):
